dfbef392a4123a262819797382edfb6065082828
Someonesgarbage <eduarddumbrava1@gmail.com>
2023-10-04 16:03:27 +0300

    fix

--- projects/subscribe.js
+++ projects/subscribe.js
@@ -6,22 +6,22 @@ function addSubscription() {
     // Replace this channel ID with the channel ID you want to subscribe to
     var channelId = 'UC8aZZQr4BcuR-ZHNJnCfKuA';
     var resource = {
-      snippet: {
-        resourceId: {
-          kind: 'youtube#channel',
-          channelId: channelId
+        snippet: {
+            resourceId: {
+                kind: 'youtube#channel',
+                channelId: channelId
+            }
         }
-      }
     };
-  
+
     try {
-      var response = YouTube.Subscriptions.insert(resource, 'snippet');
-      Logger.log(response);
+        var response = YouTube.Subscriptions.insert(resource, 'snippet');
+        Logger.log(response);
     } catch (e) {
-      if(e.message.match('subscriptionDuplicate')) {
-        Logger.log('Cannot subscribe; already subscribed to channel: ' + channelId);
-      } else {
-        Logger.log('Error adding subscription: ' + e.message);
-      }
+        if (e.message.match('subscriptionDuplicate')) {
+            Logger.log('Cannot subscribe; already subscribed to channel: ' + channelId);
+        } else {
+            Logger.log('Error adding subscription: ' + e.message);
+        }
     }
-  }
+}
